d0cdbd2b0b6b1de95570d12f4b97d978ea87c1b7
Kiem79 <24126129@student.hcmute.edu.vn>
2026-04-24 15:49:48 +0700

    Them kich ban deploy GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // BẮT BUỘC: Để nộp bài trang tĩnh và fix lỗi đường dẫn ảnh
-  //output: "export",
+  output: "export",
+
   basePath: "/nhom05_food_ordering",
   
   images: {
